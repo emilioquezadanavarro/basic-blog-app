25cd24a47033e5a2d602ad671e4ec9205434ada9
Emilio Quezada <equezadanavarro@gmail.com>
2025-11-11 15:21:17 +0100

    Function for reading json file added and new route function established

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 print("Registered routes:", app.url_map) # Adding this line for better view of the routes
 
+# Load posts from Json file.
+def load_posts():
+    try:
+        with open('posts.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("The file does not exist")
+        return []
+
+# Load the posts when the app starts
+posts = load_posts()
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    """ Pass posts to the template """
+    return render_template('index.html', posts = posts)
 
 
 if __name__ == '__main__':
